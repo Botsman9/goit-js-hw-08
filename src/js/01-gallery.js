9c7384b208b22283622d343b2767e9245b74085b
Dmitriy <dmitriy.botsman@gmail.com>
2021-11-14 13:01:21 +0200

    dz1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,37 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+
+
+const addgallery = document.querySelector(".gallery");
+
+const galleryString = galleryItems.map(({ preview, original, description }) =>
+    `<a class="gallery__item" href="${original}">
+  
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+
+</a>`).join(" ");
+
+addgallery.insertAdjacentHTML(`beforeend`, galleryString);
+
+
+
+
+addgallery.addEventListener("click", (event) => {
+event.preventDefault();
+    if (event.target.tagName !== "IMG") return;
+    
+    const origImg = event.target;
+    
+    
+    const lightbox = new SimpleLightbox('.gallery a', {captionsData:"alt",captionDelay: 250 , captionPosition: ""});
+
+    lightbox.show();
+    
+   });
